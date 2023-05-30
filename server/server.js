726218d53c888f052793a4abccf0449034480362
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-05-30 17:30:01 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const app = require('./app');
 
 const DB_HOST =
   'mongodb+srv://yuravasilenko98:8O3aoi12zjvLNhOs@cluster0.oarorn8.mongodb.net/db-delivery?retryWrites=true&w=majority';
-const PORT = 'https://delivery-app-test.herokuapp.com/';
+const PORT =
+  'https://6476079736e9b1071c336880--spiffy-jelly-c5a04d.netlify.app';
 
 mongoose.set('strictQuery', true);
 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="delivery-app-test.herokuapp.com">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
